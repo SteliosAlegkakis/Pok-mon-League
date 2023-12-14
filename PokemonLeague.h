7f02e25c1c8a06bedc54b0add480ab7db4f4ecf2
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-12-14 21:02:40 +0200

    added Ability class

--- PokemonLeague.h
+++ PokemonLeague.h
@@ -2,11 +2,13 @@
 #include <string>
 #include <iostream>
 #include <map>
+#include <functional>
 using namespace std;
 
 #define BEGIN_GAME int main(){
 #define END_GAME ;return 0;}
 #define SHOW ;cout<< 
+#define END_LINE <<"\n";
 #define CREATE ;
 #define POKEMON Pokemon
 #define NAME false?"name"
@@ -14,17 +16,26 @@ using namespace std;
 #define HP false?0
 #define DEAR Pokemon::getPokemon(
 #define LEARN ).getName();
+#define ABILITY Ability
+#define ACTION false?[](Pokemon& p1, Pokemon& p2)->void{}
+#define START [](Pokemon& attacker, Pokemon& deffender)->void{
+#define END }
+#define ABILITY_NAME(ability_name) #ability_name,
 
-class Pokemon
-{
+class Pokemon {
 private:
     std::string name;
     std::string type;
     int health_points;
     static std::map<std::string, Pokemon*> pokemons;
-    
+
 public:
-    Pokemon(std::string _name ,std::string _type, int _health_points);
+    Pokemon(std::string _name ,std::string _type, int _health_points){
+        name = _name;
+        type = _type;
+        health_points = _health_points;
+        pokemons[_name] = this;
+    }
     std::string getName() { return this->name; }
     std::string getType() { return this->type; }
     int getHealthPoints() { return this->health_points; }
@@ -34,10 +45,21 @@ public:
 
 std::map<std::string, Pokemon*> Pokemon::pokemons;
 
-Pokemon::Pokemon(std::string _name ,std::string _type, int _health_points)
-{
-    name = _name;
-    type = _type;
-    health_points = _health_points;
-    pokemons[_name] = this;
-}
+class Ability {
+private:
+    std::string name;
+    std::function<void(Pokemon&, Pokemon&)> action;
+    static std::map<std::string, Ability*> abilities;
+public:
+    using FunctionType = std::function<void(Pokemon&, Pokemon&)>;
+    Ability(std::string _name, FunctionType _action) /*: name(_name), action(std::move(_action))*/ {
+        name = _name;
+        action = _action;
+        abilities[_name] = this;
+    }
+    void execute(Pokemon& attacker, Pokemon& deffender){ action(attacker,deffender); }
+    static const std::map<std::string, Ability*> getAbilities() { return abilities; }
+    static Ability getAbility(std::string ability_name) { return *abilities[ability_name]; }
+};
+
+std::map<std::string, Ability*> Ability::abilities;
