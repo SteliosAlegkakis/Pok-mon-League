8bad303e268eda6f8908c6487b34bf7a0df3219f
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-12-26 18:34:58 +0200

    added complete example

--- app.cpp
+++ app.cpp
@@ -4,30 +4,30 @@ BEGIN_GAME
 
 CREATE POKEMON {
     NAME : "pikachu",
-    TYPE : "electric",
+    TYPE : "Electric",
     HP : 120
 }
 
 CREATE POKEMON {
     NAME : "bulbasaur",
-    TYPE : "grass",
+    TYPE : "Grass",
     HP : 120
 }
 
 CREATE POKEMONS [
     POKEMON {
         NAME : "squirtle",
-        TYPE : "water",
+        TYPE : "Water",
         HP : 120
     },
     POKEMON {
         NAME : "Ho oh",
-        TYPE : "fire",
+        TYPE : "Fire",
         HP : 120
     },
     POKEMON {
         NAME : "charmander",
-        TYPE : "fire",
+        TYPE : "Fire",
         HP : 120
     }
 ]
@@ -36,12 +36,12 @@ CREATE ABILITY {
     NAME : "electric_shock",
     ACTION : START 
         IF GET_HP(DEFENDER) == 120 DO
-            DAMAGE DEFENDER 20
+            DAMAGE DEFENDER 100
         ELSE_IF GET_HP(DEFENDER) == 100 DO
             HEAL ATTACKER 20
             DAMAGE DEFENDER 10
         ELSE
-            DAMAGE DEFENDER 5
+            DAMAGE DEFENDER 20
         END
     END
 }
@@ -67,14 +67,11 @@ CREATE ABILITIES [
     }
 ]
 
-;Pokemon::printPokemons();
-
 DEAR "pikachu" LEARN [
     ABILITY_NAME(electric_shock)
     ABILITY_NAME(bite)
     ABILITY_NAME(slash)
 ]
-;Pokemon::getPokemon("pikachu").printAbilities();
 
 DEAR "charmander" LEARN [
     ABILITY_NAME(blaze)
@@ -82,16 +79,6 @@ DEAR "charmander" LEARN [
     ABILITY_NAME(bite)
 ]
 
-;Pokemon charmander = Pokemon::getPokemon("charmander");
-Pokemon pikachu = Pokemon::getPokemon("pikachu");
-Ability::getAbility("electric_shock").execute(pikachu,charmander);
-charmander.printStatus();
-pikachu.printStatus();
-Ability::getAbility("electric_shock").execute(pikachu,charmander);
-charmander.printStatus();
-pikachu.printStatus();
-Ability::getAbility("electric_shock").execute(pikachu,charmander);
-charmander.printStatus();
-pikachu.printStatus();
+DUEL
 
 END_GAME
